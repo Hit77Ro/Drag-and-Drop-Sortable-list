ff3bb88a0f3d2d237134e834d5bc3d8eee2a10a8
Hit77Ro <smailettanjaoui@gmail.com>
2023-07-16 06:50:13 +0100

    ""

---
+++ dist/js/index.js
@@ -0,0 +1,30 @@
+let items = document.querySelectorAll(".item");
+const list = items[0].parentElement;
+console.log(list);
+
+items.forEach((draggable) => {
+  draggable.addEventListener("dragstart", (e) => {
+    setTimeout(() => draggable.classList.add("dragging"), 0);
+  });
+
+  draggable.addEventListener("dragend", (e) => {
+    draggable.classList.remove("dragging");
+  });
+});
+
+const initSortableList = (e) => {
+  e.preventDefault();
+  const draggingItem = list.querySelector(".dragging");
+  const siblings = [...list.querySelectorAll(".draggable:not(.dragging)")];
+
+  let nextSibling = siblings.find(
+    (sibling) =>
+      e.clientY <= sibling.getBoundingClientRect().y + sibling.offsetHeight / 2
+  );
+  if (nextSibling) list.insertBefore(draggingItem, nextSibling);
+  else list.append(draggingItem);
+  // we can do that without if else , just inserting before  but an undefined will print out if we drag last item
+};
+
+list.addEventListener("dragover", initSortableList);
+list.addEventListener("dragenter", (e) => e.preventDefault());
